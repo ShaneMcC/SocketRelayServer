68b6d05b3254e7886a1275fd5d11f3d97e23cbed
Shane Mc Cormack <shanemcc@gmail.com>
2017-09-18 23:28:15 +0100

    Dockerfile

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	$config['listen']['port'] = getEnvOrDefault('LISTEN_PORT', '3302');
 	$config['listen']['timeout'] = getEnvOrDefault('LISTEN_TIMEOUT', '10');
 
-	$config['failFile'] = __DIR__ . '/.failedMessages';
+	$config['failFile'] = getEnvOrDefault('FAILFILE', __DIR__ . '/.failedMessages');
 
 	$config['reporthandler'] = 'socketrelay';
 	$config['reporter']['socketrelay']['host'] = getEnvOrDefault('REPORTER_SOCKETRELAY_HOST', 'somehost');
@@ -16,6 +16,5 @@
 	$config['validKeys']['347FF0B5-35BB-4212-A78B-67883B6F2A97'] = ['*'];
 	$config['validKeys']['F1DD172A-233E-4CEF-8787-36D6E6C8C931'] = ['CM'];
 
-	if (file_exists(dirname(__FILE__) . '/config.local.php')) {
-		include(dirname(__FILE__) . '/config.local.php');
-	}
+	$localconf = getEnvOrDefault('LOCALCONF', __DIR__ . '/config.local.php');
+	if (file_exists($localconf)) { include($localconf); }
